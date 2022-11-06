8cdc0dd0ed45ebe014ae74585522a33f1e34048a
Ole Mussmann <gitlab+account@ole.mn>
2022-11-06 14:52:18 +0100

    tweak package list

--- configuration.nix
+++ configuration.nix
@@ -127,10 +127,10 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    neovim
     ncdu
+    tree
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
