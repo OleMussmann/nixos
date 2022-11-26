e3472c3c4020e9c1dcc9b3c7b0490f8d21099099
Ole Mussmann <gitlab+account@ole.mn>
2022-11-26 19:29:41 +0100

    enable hostname

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -22,7 +22,7 @@
   fileSystems."/home".neededForBoot = true;
   fileSystems."/persist".neededForBoot = true;
 
-  # networking.hostName = "nixos-vm"; # Define your hostname.
+  networking.hostName = "nixos-vm"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
