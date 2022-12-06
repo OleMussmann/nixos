69a0ed38284202e0c57aac9aa1e4c28c4426eb48
ole <gitlab+account@ole.mn>
2022-12-06 12:03:37 +0100

    persist more things

--- hosts/shared.nix
+++ hosts/shared.nix
@@ -12,6 +12,22 @@
       wget
     ];
 
+  # machine-id is used by systemd for the journal, if you don't
+  # persist this file you won't be able to easily use journalctl to
+  # look at journals for previous boots.
+  #
+  # If you want to run an openssh daemon, you may want to store the
+  # host keys across reboots. For this to work you will need to
+  # create the directory yourself:
+  # $ mkdir /persist/etc/ssh
+  environment.etc = {
+    "machine-id".source = "/persist/etc/machine-id";
+    "ssh/ssh_host_rsa_key".source = "/persist/etc/ssh/ssh_host_rsa_key";
+    "ssh/ssh_host_rsa_key.pub".source = "/persist/etc/ssh/ssh_host_rsa_key.pub";
+    "ssh/ssh_host_ed25519_key".source = "/persist/etc/ssh/ssh_host_ed25519_key";
+    "ssh/ssh_host_ed25519_key.pub".source = "/persist/etc/ssh/ssh_host_ed25519_key.pub";
+  };
+
   nix = {                                   # Nix Package Manager settings
     settings ={
       auto-optimise-store = true;           # Optimise syslinks
