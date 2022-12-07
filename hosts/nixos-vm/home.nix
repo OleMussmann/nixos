3880d5b53759afa9dbe0592438d4b71096e9c7cb
ole <gitlab+account@ole.mn>
2022-12-07 14:38:25 +0100

    set keybinds

--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -63,6 +63,15 @@
     "org/gnome/settings-daemon/plugins/power" = {
       sleep-inactive-ac-type = "nothing";
     };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Control><Alt>t";
+      command = "kgx";
+      name = "Launch Console";
+    };
   };
 
   programs.zsh = {
