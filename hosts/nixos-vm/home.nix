86357e59476b2b542c484ba6ea5a8d934e9b58ca
ole <gitlab+account@ole.mn>
2022-12-07 08:18:14 +0100

    disable starship theming (for now)

--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -110,17 +110,17 @@
     enable = true;
     settings = {
       add_newline = false;
-      format = lib.concatStrings [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
-      ];
-      scan_timeout = 10;
-      character = {
-        success_symbol = "➜";
-        error_symbol = "➜";
-      };
+      #format = lib.concatStrings [
+      #  "$line_break"
+      #  "$package"
+      #  "$line_break"
+      #  "$character"
+      #];
+      #scan_timeout = 10;
+      #character = {
+      #  success_symbol = "➜";
+      #  error_symbol = "➜";
+      #};
     };
   };
 
