4da236f881d082049c131e2952cd41c84f2a62f5
Ole Mussmann <gitlab+account@ole.mn>
2023-01-12 10:05:15 +0100

    switch to nps development branch

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Nix Package Search
-    #nps.url = "github:OleMussmann/Nix-Package-Search";
-    nps.url = "path:/home/ole/bin/Nix-Package-Search";
+    nps.url = "github:OleMussmann/Nix-Package-Search/development";
+    #nps.url = "path:/home/ole/bin/Nix-Package-Search";
     nps.inputs.nixpkgs.follows = "nixpkgs";
 
     # Impermanence
