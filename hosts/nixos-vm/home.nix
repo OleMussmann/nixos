dee2176d9032c66835649c7e9b07ec0a6828e216
ole <gitlab+account@ole.mn>
2023-02-27 20:30:30 +0100

    extend starship for python environments

--- hosts/nixos-vm/home.nix
+++ hosts/nixos-vm/home.nix
@@ -36,6 +36,7 @@ in {
       gimp             # Graphical editor
       handbrake        # Encoder
       inkscape         # Vector graphical editor
+      jq               # json parser
       keepassxc        # Password manager
       libreoffice      # Office packages
       logseq           # knowledge base
@@ -214,6 +215,16 @@ in {
           error_symbol = " [](#df5b61)";
           vicmd_symbol = "[  ](#78b892)";
         };
+	custom = {
+	  direnv = {
+	    command = "nix flake metadata --json 2>/dev/null | jq '.description' | sed -e 's/\"//g'; if [ \"\$\{PIPESTATUS[0]\}\" != \"0\" ]; then basename \"$PWD\"; fi ";
+	    shell = "bash";
+	    format = "[$symbol$output]($style)";
+	    style = "fg:bold green";
+	    symbol = "📁 ";
+	    when = "env | grep -E '^DIRENV_FILE='";
+	  };
+	};
         hostname = {
           ssh_only = true;
           format = "[$hostname](bold blue) ";
