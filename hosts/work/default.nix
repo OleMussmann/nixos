a3c16ca77e6b1e3444f5322d450196e2cfdad379
Ole Mussmann <gitlab+account@ole.mn>
2023-04-16 16:52:00 +0200

    add KDE partition manager

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -129,10 +129,15 @@
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
+  programs = {
+    mtr.enable = true;
+
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+
+    partition-manager.enable = true;
   };
 
   # List services that you want to enable:
