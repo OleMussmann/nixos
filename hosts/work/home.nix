0322527b5f23bbf1aad5d3b1d50780ff17ad3e53
Ole Mussmann <gitlab+account@ole.mn>
2023-05-01 08:49:17 +0200

    add cookiecutter

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -15,6 +15,7 @@ in {
       any-nix-shell    # use fish in nix-shell
       appimage-run     # Runs AppImages on NixOS
       comma            # run programs without installing them
+      cookiecutter     # coding templates
       chromium         # Browser
       dconf2nix        # turn GNOME dconf settings to nix strings
       discord          # Comms
