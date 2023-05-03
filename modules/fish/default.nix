f55f1125479e0f71ad8e54ee9c2964966f90bad4
Ole Mussmann <gitlab+account@ole.mn>
2023-05-03 15:17:30 +0200

    make upgrade alias more generic

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -31,7 +31,7 @@
         shellAliases = {
           trash = "gio trash";
           update = "nix flake update --commit-lock-file /home/ole/.system";
-          upgrade = "sudo nixos-rebuild switch --flake /home/ole/.system#nixos-vm &&
+          upgrade = "sudo nixos-rebuild switch --flake /home/ole/.system &&
             nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -n 2)";
         };
         interactiveShellInit = ''
