8d565e3c6537b790e2566d5a7e2d8378ba5f6e9d
Ole Mussmann <gitlab+account@ole.mn>
2023-05-07 17:58:05 +0200

    switch to ext4 for /boot

--- hosts/pintsize/partitioning_and_first_install.sh
+++ hosts/pintsize/partitioning_and_first_install.sh
@@ -29,7 +29,7 @@ echo "# create (temporary) root password #"
 echo "####################################"
 echo
 
-rootPwd=$(mkpasswd -m SHA-512)
+#rootPwd=$(mkpasswd -m SHA-512)
 
 # Enable Nix Flakes functionality.
 mkdir -p ~/.config/nix
@@ -47,7 +47,7 @@ partition_disk () {
  parted --script --align=optimal "${disk}" -- \
  mklabel gpt \
  mkpart EFI 2MiB 1GiB \
- mkpart bpool 1GiB 5GiB \
+ mkpart boot 1GiB 5GiB \
  mkpart rpool 5GiB -$((SWAPSIZE + RESERVE))GiB \
  mkpart swap -$((SWAPSIZE + RESERVE))GiB -"${RESERVE}"GiB \
  mkpart BIOS 1MiB 2MiB \
@@ -82,30 +82,34 @@ for i in ${DISKS}; do
 done
 
 echo
-echo "####################"
-echo "# create boot pool #"
-echo "####################"
+echo "#########################"
+echo "# create boot partition #"
+echo "#########################"
 echo
 
-# shellcheck disable=SC2046
-zpool create \
-    -o compatibility=grub2 \
-    -o ashift=12 \
-    -o autotrim=on \
-    -O acltype=posixacl \
-    -O canmount=off \
-    -O compression=lz4 \
-    -O devices=off \
-    -O normalization=formD \
-    -O relatime=on \
-    -O xattr=sa \
-    -O mountpoint=/boot \
-    -R "${MNT}" \
-    bpool \
-  mirror \
-    $(for i in ${DISKS}; do
-       printf '%s ' "${i}-part2";
-      done)
+for i in ${DISKS}; do
+  mkfs.ext4 "${i}-part2"
+done
+
+## shellcheck disable=SC2046
+#zpool create \
+#    -o compatibility=grub2 \
+#    -o ashift=12 \
+#    -o autotrim=on \
+#    -O acltype=posixacl \
+#    -O canmount=off \
+#    -O compression=lz4 \
+#    -O devices=off \
+#    -O normalization=formD \
+#    -O relatime=on \
+#    -O xattr=sa \
+#    -O mountpoint=/boot \
+#    -R "${MNT}" \
+#    bpool \
+#  mirror \
+#    $(for i in ${DISKS}; do
+#       printf '%s ' "${i}-part2";
+#      done)
 
 echo
 echo "####################"
@@ -181,11 +185,11 @@ zfs create -o mountpoint=legacy rpool/nixos/var/log
 mkdir -p "${MNT}"/var/log
 mount -t zfs rpool/nixos/var/log "${MNT}"/var/log
 
-# boot
-zfs create -o mountpoint=none bpool/nixos
-zfs create -o mountpoint=legacy bpool/nixos/root
+## boot
+#zfs create -o mountpoint=none bpool/nixos
+#zfs create -o mountpoint=legacy bpool/nixos/root
 mkdir "${MNT}"/boot
-mount -t zfs bpool/nixos/root "${MNT}"/boot
+mount -t ext4 "${DISKS[0]}-part2" "${MNT}"/boot
 
 echo
 echo "########################"
@@ -199,105 +203,105 @@ for i in ${DISKS}; do
  mount -t vfat -o iocharset=iso8859-1 "${i}"-part1 "${MNT}"/boot/efis/"${i##*/}"-part1
 done
 
-echo
-echo "###############################"
-echo "# clone flake template config #"
-echo "###############################"
-echo
+#echo
+#echo "###############################"
+#echo "# clone flake template config #"
+#echo "###############################"
+#echo
+#
+#mkdir -p "${MNT}"/etc
+#git clone --depth 1 --branch openzfs-guide \
+#  https://github.com/ne9z/dotfiles-flake.git "${MNT}"/etc/nixos
+#
+#echo
+#echo "######################"
+#echo "# customize hardware #"
+#echo "######################"
+#echo
+#
+#for i in ${DISKS}; do
+#  sed -i \
+#  "s|/dev/disk/by-id/|${i%/*}/|" \
+#  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+#  break
+#done
+#
+#diskNames=""
+#for i in ${DISKS}; do
+#  diskNames="${diskNames} \"${i##*/}\""
+#done
+#
+#sed -i "s|\"bootDevices_placeholder\"|${diskNames}|g" \
+#  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+#
+#sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" \
+#  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+#
+#sed -i "s|\"x86_64-linux\"|\"$(uname -m || true)-linux\"|g" \
+#  "${MNT}"/etc/nixos/flake.nix
+#
+#cp "$(command -v nixos-generate-config || true)" ./nixos-generate-config
+#
+#chmod a+rw ./nixos-generate-config
+#
+## shellcheck disable=SC2016
+#echo 'print STDOUT $initrdAvailableKernelModules' >> ./nixos-generate-config
+#
+#kernelModules="$(./nixos-generate-config --show-hardware-config --no-filesystems | tail -n1 || true)"
+#
+#sed -i "s|\"kernelModules_placeholder\"|${kernelModules}|g" \
+#  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
+#
+#echo
+#echo "#################################"
+#echo "# set (temporary) root password #"
+#echo "#################################"
+#echo
+#
+#sed -i \
+#"s|rootHash_placeholder|${rootPwd}|" \
+#"${MNT}"/etc/nixos/configuration.nix
+#
+#echo
+#echo "#########################"
+#echo "# enable networkmanager #"
+#echo "#########################"
+#echo
+#
+#sed -i -e "/networkmanager/ s|false|true|" "${MNT}"/etc/nixos/configuration.nix
+#
+#echo
+#echo "#####################"
+#echo "# update flake.lock #"
+#echo "#####################"
+#echo
+#
+#nix flake update
+#
+#echo
+#echo "##################"
+#echo "# install system #"
+#echo "##################"
+#echo
 
-mkdir -p "${MNT}"/etc
-git clone --depth 1 --branch openzfs-guide \
-  https://github.com/ne9z/dotfiles-flake.git "${MNT}"/etc/nixos
-
-echo
-echo "######################"
-echo "# customize hardware #"
-echo "######################"
-echo
-
-for i in ${DISKS}; do
-  sed -i \
-  "s|/dev/disk/by-id/|${i%/*}/|" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
-  break
-done
-
-diskNames=""
-for i in ${DISKS}; do
-  diskNames="${diskNames} \"${i##*/}\""
-done
-
-sed -i "s|\"bootDevices_placeholder\"|${diskNames}|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
-
-sed -i "s|\"abcd1234\"|\"$(head -c4 /dev/urandom | od -A none -t x4| sed 's| ||g' || true)\"|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
-
-sed -i "s|\"x86_64-linux\"|\"$(uname -m || true)-linux\"|g" \
-  "${MNT}"/etc/nixos/flake.nix
-
-cp "$(command -v nixos-generate-config || true)" ./nixos-generate-config
-
-chmod a+rw ./nixos-generate-config
-
-# shellcheck disable=SC2016
-echo 'print STDOUT $initrdAvailableKernelModules' >> ./nixos-generate-config
-
-kernelModules="$(./nixos-generate-config --show-hardware-config --no-filesystems | tail -n1 || true)"
-
-sed -i "s|\"kernelModules_placeholder\"|${kernelModules}|g" \
-  "${MNT}"/etc/nixos/hosts/exampleHost/default.nix
-
-echo
-echo "#################################"
-echo "# set (temporary) root password #"
-echo "#################################"
-echo
-
-sed -i \
-"s|rootHash_placeholder|${rootPwd}|" \
-"${MNT}"/etc/nixos/configuration.nix
-
-echo
-echo "#########################"
-echo "# enable networkmanager #"
-echo "#########################"
-echo
-
-sed -i -e "/networkmanager/ s|false|true|" "${MNT}"/etc/nixos/configuration.nix
-
-echo
-echo "#####################"
-echo "# update flake.lock #"
-echo "#####################"
-echo
-
-nix flake update
-
-echo
-echo "##################"
-echo "# install system #"
-echo "##################"
-echo
-
-nixos-install \
---root "${MNT}" \
---no-root-passwd \
---flake "git+file://${MNT}/etc/nixos#exampleHost"
-
-echo
-echo "#######################"
-echo "# unmount filesystems #"
-echo "#######################"
-echo
-
-umount -Rl "${MNT}"
-zpool export -a
-
-echo
-echo "##########"
-echo "# reboot #"
-echo "##########"
-echo
-
-reboot
+#nixos-install \
+#--root "${MNT}" \
+#--no-root-passwd \
+#--flake "git+file://${MNT}/etc/nixos#exampleHost"
+#
+#echo
+#echo "#######################"
+#echo "# unmount filesystems #"
+#echo "#######################"
+#echo
+#
+#umount -Rl "${MNT}"
+#zpool export -a
+#
+#echo
+#echo "##########"
+#echo "# reboot #"
+#echo "##########"
+#echo
+#
+#reboot
