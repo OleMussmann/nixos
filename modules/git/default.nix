f7431f8a44c32098a76a5daaa0569add0bf8ef25
ole <gitlab+account@ole.mn>
2023-05-26 22:50:23 +0200

    add git options

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -6,6 +6,14 @@
       enable = true;
       userName  = "Ole Mussmann";
       userEmail = "gitlab+account@ole.mn";
+      diff-so-fancy.enable = true;
+      extraConfig = {
+        init.defaultBranch = "main";
+        diff.tool = "vimdiff3";
+        difftool.vimdiff3.path = "nvim";
+        merge.tool = "vimdiff";
+        merge.conflictStyle = "diff3";
+      };
     };
   };
 }
