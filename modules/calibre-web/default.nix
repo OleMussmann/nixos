300b5e4456ebb5ddfce64b2466655d78038250a3
Ole Mussmann <gitlab+account@ole.mn>
2023-08-04 20:40:25 +0200

    tweak avahi

--- modules/calibre-web/default.nix
+++ modules/calibre-web/default.nix
@@ -17,14 +17,14 @@
   services.avahi = {
     enable = true;
     nssmdns = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
+    #publish = {
+    #  enable = true;
+    #  addresses = true;
+    #  domain = true;
+    #  hinfo = true;
+    #  userServices = true;
+    #  workstation = true;
+    #};
   };
   services.nginx = {
     enable = true;
